1bb82947fda379019fa581f4ad0568e94451df38
Tmrbot4 <166976733+Tmrbot4@users.noreply.github.com>
2024-05-13 20:17:35 +0530

    Update info.py

--- info.py
+++ info.py
@@ -50,8 +50,8 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", True)) # True if you want no results messages in Log Channel
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Clone2: Clon2@cluster0.wnj9p4u.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Clone2")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Tmrbot:Tmrbot@cluster0.reewfm0.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Tmrbot")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Verify
